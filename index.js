a369f7667d5fda805699c7bdb843bbeda192ad95
Neto <j.altevirneto@gmail.com>
2024-06-12 11:32:52 -0300

    fix feat: pergunta/resposta

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let entrada_usuario = "";
 console.log("Você toca algum instrumento?");
 process.stdin.on("data", function (data){
     entrada_usuario = data.toString().trim().toLowerCase();
-    if(entrada_usuario == s || entrada_usuario == sim){
+    if(entrada_usuario == "s" || entrada_usuario == "sim"){
         console.log("Parabéns! Você é um artista! =D");
         process.exit();
-    } else if(entrada_usuario == não || entrada_usuario == n || entrada_usuario == nao){
+    } else if(entrada_usuario == "não" || entrada_usuario == "n" || entrada_usuario == "nao"){
         console.log("Já pensou em aprender algum?")
         process.exit();
     }
