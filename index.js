0573ffb079eb92aae62646b0545cb687dfee2c90
Neto <j.altevirneto@gmail.com>
2024-06-13 11:13:38 -0300

    fix: codigo escalavel

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-let entrada_usuario = "";
+let entrada_usuario;
 console.log("Você toca algum instrumento?");
 process.stdin.on("data", function (data){
-    entrada_usuario = data.toString().trim().toLowerCase();
-    if(entrada_usuario == "s" || entrada_usuario == "sim"){
-        console.log("Parabéns! Você é um artista! =D");
-        process.exit();
-    } else if(entrada_usuario == "não" || entrada_usuario == "n" || entrada_usuario == "nao"){
-        console.log("Já pensou em aprender algum?")
-        process.exit();
+    let input = data.toString().trim().toLowerCase();
+    if(!entrada_usuario){
+        entrada_usuario = input;
+        if(entrada_usuario == "s" || entrada_usuario == "sim"){
+            console.log("Parabéns! Você é um artista! =D");
+        } else if(entrada_usuario == "não" || entrada_usuario == "n" || entrada_usuario == "nao"){
+            console.log("Já pensou em aprender algum?")
+            process.exit();
+        } else {
+            console.log("Opção não válida, digite novamente:");
+            entrada_usuario;
+        }
     }
 })
