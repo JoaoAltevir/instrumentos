7059b9432aa3e253b8c3621851e917bfb9aa3845
Neto <j.altevirneto@gmail.com>
2024-06-17 09:27:03 -0300

    feat: add instrumento novo na lista

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 let entrada_usuario = "";
 let instrumento = [{nome: 'violao', frase: 'modão e bar, combinação perfeita!'},];
 let instrumentoUsuario = "";
-console.log("Você toca algum instrumento?");
+let instrumentoNovo = false;
+let mensagemInicial = "Você toca algum instrumento?"
+console.log(mensagemInicial);
 process.stdin.on("data", function (data){
     let input = data.toString().trim().toLowerCase();
-    if(!entrada_usuario){
-        entrada_usuario = input;
-        if(entrada_usuario == "s" || entrada_usuario == "sim"){
-            console.log("Qual? (digite sem acentuação)");
-        } else if(entrada_usuario == "não" || entrada_usuario == "n" || entrada_usuario == "nao"){
-            console.log("Já pensou em aprender algum?")
-            process.exit();
+    if(instrumentoNovo == false){
+        if(!entrada_usuario){
+            entrada_usuario = input;
+            if(entrada_usuario == "s" || entrada_usuario == "sim"){
+                console.log("Qual? (digite sem acentuação)");
+            } else if(entrada_usuario == "não" || entrada_usuario == "n" || entrada_usuario == "nao"){
+                console.log("Já pensou em aprender algum?")
+                process.exit();
+            } else {
+                console.log("Opção não válida, digite novamente:");
+                entrada_usuario = "";
+            }
         } else {
-            console.log("Opção não válida, digite novamente:");
-            entrada_usuario = "";
+            if(!instrumentoUsuario){
+                instrumentoUsuario = input;
+                for(let i = 0; i < instrumento.length;i++){
+                    if(instrumentoUsuario == instrumento[i].nome){
+                        console.log(instrumento[i].frase);
+                    }else{
+                        instrumento.push({nome: instrumentoUsuario, frase: ''});
+                        instrumentoNovo = true;
+                        console.log("Instrumento não encontrado! Porém adicionamos ele na nossa lista, que frase gostaria que aparecesse caso digitasse este instrumento?")
+                    }
+                }
+            }
         }
     } else {
-        if(!instrumentoUsuario){
-            instrumentoUsuario = input;
-            for(let i = 0; i < instrumento.length;i++){
-                if(instrumentoUsuario == instrumento[i].nome){
-                    console.log(instrumento[i].frase);
-                    process.exit();
-                }else{
-                    instrumento.push({nome: instrumentoUsuario, frase: ''});
-                    console.log(instrumento)
-                    console.log("Instrumento não encontrado! Porém adicionamos ele na nossa lista, que frase gostaria que aparecesse caso digitasse este instrumento?")
-                }
+        for(let i = 0; i < instrumento.length;i++){
+            if(!instrumento[i].frase){
+                instrumento[i].frase = input;
+                instrumentoNovo = false
+                instrumentoUsuario = "";
+                entrada_usuario = "";
+                console.log(mensagemInicial)
             }
         }
     }
